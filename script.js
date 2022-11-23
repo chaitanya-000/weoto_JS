7b18746780e9a5b9143ce7841ae4eb893603327b
Chaitanya Joshi <Chaitanya99j@gmail.com>
2022-11-23 12:58:57 +0530

    temporal dead zone

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// let a = 1000;
+let a = 1000;
 
-// console.log(a);
+console.log(a);
 
-// var b = 2000;
+var b = 2000;
 
 //  Time between variable is hoisted - variable is assigned
 //Temporal dead zone - Anything before line 3 is the temporal dead zone of a
 
 // var variable’s Temporal dead zone ends immediately after its hoisting—not when the variable gets fully initialized
 // with the value specified during its declaration. Just opposite  case with let and const
+
+//0
+
+// let and const are also hoisted. But an exception will be thrown if a variable declared with let or const is read before it is initialised due to below reasons.
+
+// Unlike var, they are not initialised with a default value while hoisting.
+// They cannot be read/written until they have been fully initialised.
 
 //The scope of name inside the greet is within its block
 const name = "Mark";
